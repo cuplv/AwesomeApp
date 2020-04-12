f9da7092b4f0165fbe09fa298f783051bf4ef123
Sergio Mover <sergio.mover@gmail.com>
2020-04-12 13:50:44 +0200

    Fix bug

--- app/src/main/java/fixr/plv/colorado/edu/awesomeapp/MainActivity.java
+++ app/src/main/java/fixr/plv/colorado/edu/awesomeapp/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       });
 
     dialogBuilder.create();
-    // dialogBuilder.show();
+    dialogBuilder.show();
   }
 
     @Override
